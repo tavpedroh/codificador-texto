22d36eb9f4211fa493e299b5b3f6e70eb030f00d
Pedro Tavares <pedroh.tav.p@gmail.com>
2024-07-06 17:05:13 -0300

    inicio codigo fonte

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-
-
+let texto;
+let textoCod;
+let i=0,j=10;
+function criptografar(){
+    texto = document.querySelector('input').value;
+    for(i=0;i<100;i++){
+        textocod[j] = texto[i];
+        j--;
+    }
+    
+}
