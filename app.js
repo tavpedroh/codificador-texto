7728f5a5eeea55063fc0e31129719d28e26c7a74
Pedro Tavares <pedroh.tav.p@gmail.com>
2024-07-08 10:35:36 -0300

    exibir texto na tela

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-let texto;
-let textoCod;
-let i=0,j=10;
-function criptografar(){
-    texto = document.querySelector('input').value;
-    for(i=0;i<100;i++){
-        textocod[j] = texto[i];
-        j--;
-    }
-    
+function exibirTextoNaTela(tag,texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
+//exibirTextoNaTela('h',texto);
+let text = document.querySelector(entrada);
+
