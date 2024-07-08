fb8d83298ba7c57da6eb1f3de1bd26c16c8b088d
Pedro Tavares <pedroh.tav.p@gmail.com>
2024-07-08 10:54:46 -0300

    inicio de uma tentativa de criptografia

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+const substituicoes ={
+    'a' : 'ai',
+    'e' : 'enter',
+    'i' : 'imes',
+    'o' : 'ober',
+    'u' : 'ufat'
+};
+
 function exibirTextoNaTela(tag,texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 //exibirTextoNaTela('h',texto);
-let text = document.querySelector(entrada);
+let input = document.querySelector(entrada);
+let text = input.value;
 
+function codificar(texto){
+    let textoCriptografado = substituirletras(texto);
+}
+
+
+function criptografar(text){
+    
+}
